ce89c4c8d12faf149da4dc8d0f098f57bc0b2019
LeonUraltsev <leon.uraltsev@gmail.com>
2021-02-27 12:00:01 +0400

    Создание Navigation Drawer, toolbar и изменение темы приложения

--- app/src/main/java/com/example/myapplicationtg/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationtg/MainActivity.kt
@@ -2,14 +2,74 @@ package com.example.myapplicationtg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.myapplicationtg.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawer: Drawer
+    private lateinit var header: AccountHeader
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    //функция отвечающая за функциональность
+    private fun initFunc() {
+        setSupportActionBar(toolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    //Создание Navigation Drawer
+    private fun createDrawer() {
+        drawer = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .withSelectedItem(-1)
+                .withAccountHeader(header)
+                .addDrawerItems(
+                        PrimaryDrawerItem()
+                                .withIdentifier(100)
+                                .withIconTintingEnabled(true)
+                                .withName("Create group")
+                                .withSelectable(false)
+                )
+                .build()
+    }
+    //Создание заголовка для Navigation Drawer
+    private fun createHeader() {
+        header = AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        ProfileDrawerItem()
+                                .withName("Leonid Uraltsev")
+                                .withEmail("uraltsev@gmail.com")
+
+                ).build()
+    }
+
+    //функция отвечающая за инициализацию полей
+    private fun initFields() {
+        toolbar = binding.mainToolbar
     }
 }
